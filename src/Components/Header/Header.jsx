527990f4d8e255df49e46bba4b6094ad1b293bd5
Fida Hussain <hyderfida14@gmail.com>
2024-05-23 12:34:02 +0530

    modified header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -6,66 +6,7 @@ import Nav from "react-bootstrap/Nav";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import logoCapital from '../../utils/images/logoCapital3.png';
-import './header.css';
-import logoCapital3 from "../../utils/images/logoCapital3.png";
-
-
-const megaMenuSections = [
-  {
-    header: "Get Started",
-    links: ["OVERVIEW", "WHY IUST?", "SCHOLARSHIP", "EDUCATIONAL LOAN"],
-  },
-  {
-    header: "ADMISSIONS",
-    links: ["OVERVIEW", "WHY IUST?", "SCHOLARSHIP", "EDUCATIONAL LOAN"],
-  },
-  {
-    header: "RESEARCH",
-    links: ["OVERVIEW", "WHY IUST?", "SCHOLARSHIP", "EDUCATIONAL LOAN"],
-  },
-  {
-    header: "ACADEMICS",
-    links: [
-      "Graphics",
-      "Business Cards",
-      "Custom Logo",
-      "Graphics",
-      "Business Cards",
-      "Custom Logo",
-    ],
-  },
-  {
-    header: "RESIDENTIAL FACILITIES",
-    links: ["TRANSPORT FACILITIES", "FLATS", "HOSTEL"],
-  },
-];
-
-const ourCoursesSections = [
-  {git
-    header: "Get Started",
-    links: ["Introducing our courses"],
-  },
-  {
-    header: "Courses",
-    links: ["Master of Studies ","(MSt),Master of Science (MSc)"],
-  },
-  // {
-  //   links: ["Master of Science (MSc)"],
-  // },
-];
-
-const MegaMenuSection = ({ header, links }) => (
-  <div className="row">
-    <header>{header}</header>
-    <ul className="mega-links">
-      {links.map((link, index) => (
-        <li key={index}>
-          <Link to="#">{link}</Link>
-        </li>
-      ))}
-    </ul>
-  </div>
-);
+import './Header.css';
 
 const Header = () => {
   const [showCourses, setShowCourses] = useState(false);
@@ -94,65 +35,6 @@ const Header = () => {
           <Link to="/" className="navbar-brand d-flex align-items-center">
             <div>
               <img src={logoCapital} alt="iust Logo" width="250px" height="50" className="" />
-    <nav>
-      <div className="wrapper">
-        <div className="logo ">
-          {/* <Link to="#">
-             {logoCapital3}
-          </Link> */}
-        </div>
-        <div className="menu-btn" onClick={toggleMenu}>
-          <i className="fas fa-bars"></i>
-        </div>
-        <ul className={`nav-links ${menuOpen ? "open" : ""}`}>
-          <div className="close-btn" onClick={toggleMenu}>
-            <i className="fas fa-times"></i>
-          </div>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link className="desktop-item">Our Courses</Link>
-           
-            <div className="mega-box">
-              <div className="content">
-                {ourCoursesSections.map((section, index) => (
-                  <CoursesMenuSection key={index} {...section} />
-                ))}
-              </div>
-            </div>
-          </li>
-          <li>
-            <Link to="/blog">Blog</Link>
-          </li>
-
-          <li>
-            <Link to="login" className="desktop-item">
-              LOGIN
-            </Link>
-            <ul className="drop-menu">
-              <a href="https://studentservice.iust.ac.in/Account/login">
-               
-                Student
-              </a>
-              <a href="https://ums.iust.ac.in/login.aspx"> Employee</a>
-            </ul>
-          </li>
-          <li>
-            <Link to="#" className="desktop-item">
-              Mega Menu
-            </Link>
-          
-          
-            <div className="mega-box">
-              <div className="content">
-                {megaMenuSections.map((section, index) => (
-                  <MegaMenuSection key={index} {...section} />
-                ))}
-              </div>
             </div>
           </Link>
         </Navbar.Brand>
@@ -198,7 +80,7 @@ const Header = () => {
               <Nav.Link as={Link} to="/login">LogIn</Nav.Link>
               {showLoginOptions && (
                 <div className="login-dropdown">
-                  <Button variant="primary" className="login-btn" >Student</Button>
+                  <Button variant="primary" className="login-btn">Student</Button>
                   <Button variant="secondary" className="login-btn">Employee</Button>
                 </div>
               )}
