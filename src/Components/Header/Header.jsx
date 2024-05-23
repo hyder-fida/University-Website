de12a061593b91a7119dfbafc8737db34ec1538c
Fida Hussain <hyderfida14@gmail.com>
2024-05-23 12:18:48 +0530

    updated header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,121 +1,94 @@
 import React, { useState } from "react";
-import "./header.css";
 import { Link } from "react-router-dom";
-import logoCapital3 from "../../utils/images/logoCapital3.png";
-
-
-const megaMenuSections = [
-  {
-    header: "Get Started",
-    links: ["OVERVIEW", "WHY IUST?", "SCHOLARSHIP", "EDUCATIONAL LOAN"],
-  },
-  {
-    header: "ADMISSIONS",
-    links: ["OVERVIEW", "WHY IUST?", "SCHOLARSHIP", "EDUCATIONAL LOAN"],
-  },
-  {
-    header: "RESEARCH",
-    links: ["OVERVIEW", "WHY IUST?", "SCHOLARSHIP", "EDUCATIONAL LOAN"],
-  },
-  {
-    header: "ACADEMICS",
-    links: [
-      "Graphics",
-      "Business Cards",
-      "Custom Logo",
-      "Graphics",
-      "Business Cards",
-      "Custom Logo",
-    ],
-  },
-  {
-    header: "RESIDENTIAL FACILITIES",
-    links: ["TRANSPORT FACILITIES", "FLATS", "HOSTEL"],
-  },
-];
-
-const MegaMenuSection = ({ header, links }) => (
-  <div className="row">
-    <header>{header}</header>
-    <ul className="mega-links">
-      {links.map((link, index) => (
-        <li key={index}>
-          <Link to="#">{link}</Link>
-        </li>
-      ))}
-    </ul>
-  </div>
-);
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import logoCapital from '../../utils/images/logoCapital3.png';
+import './header.css';
 
 const Header = () => {
-  const [menuOpen, setMenuOpen] = useState(false);
+  const [showCourses, setShowCourses] = useState(false);
+  const [showLoginOptions, setShowLoginOptions] = useState(false);
 
-  const toggleMenu = () => {
-    setMenuOpen(!menuOpen);
+  const handleMouseEnterCourses = () => {
+    setShowCourses(true);
+  };
+
+  const handleMouseLeaveCourses = () => {
+    setShowCourses(false);
+  };
+
+  const handleMouseEnterLogin = () => {
+    setShowLoginOptions(true);
+  };
+
+  const handleMouseLeaveLogin = () => {
+    setShowLoginOptions(false);
   };
 
   return (
-    <nav>
-      <div className="wrapper">
-        <div className="logo ">
-          {/* <Link to="#">
-             {logoCapital3}
-          </Link> */}
-        </div>
-        <div className="menu-btn" onClick={toggleMenu}>
-          <i className="fas fa-bars"></i>
-        </div>
-        <ul className={`nav-links ${menuOpen ? "open" : ""}`}>
-          <div className="close-btn" onClick={toggleMenu}>
-            <i className="fas fa-times"></i>
-          </div>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="courses">Our Courses</Link>
-          </li>
-          <li>
-            <Link to="/blog">Blog</Link>
-          </li>
-
-          <li>
-            <Link to="login" className="desktop-item">
-              LOGIN
-            </Link>
-            <ul className="drop-menu">
-              <a href="https://studentservice.iust.ac.in/Account/login">
-               
-                Student
-              </a>
-              <a href="https://ums.iust.ac.in/login.aspx"> Employee</a>
-            </ul>
-          </li>
-          <li>
-            <Link to="#" className="desktop-item">
-              Mega Menu
-            </Link>
-            <input type="checkbox" id="showMega" />
-            <label htmlFor="showMega" className="mobile-item">
-              Mega Menu
-            </label>
-            <div className="mega-box">
-              <div className="content">
-                {megaMenuSections.map((section, index) => (
-                  <MegaMenuSection key={index} {...section} />
-                ))}
-              </div>
+    <Navbar expand="lg" className="w-100 nav">
+      <Container>
+        <Navbar.Brand>
+          <Link to="/" className="navbar-brand d-flex align-items-center">
+            <div>
+              <img src={logoCapital} alt="iust Logo" width="250px" height="50" className="" />
             </div>
-          </li>
-          <li>
-            <Link to="/notifications">Notifications</Link>
-          </li>
-        </ul>
-      </div>
-    </nav>
+          </Link>
+        </Navbar.Brand>
+        <Navbar.Toggle
+          aria-controls="basic-navbar-nav"
+          className="bg-light"
+        />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mx-auto justify-content-end w-100 font">
+            <Nav.Link as={Link} to="/" className="text-uppercase">Home</Nav.Link>
+            <Nav.Item
+              className="text-uppercase"
+              onMouseEnter={handleMouseEnterCourses}
+              onMouseLeave={handleMouseLeaveCourses}
+            >
+              <Nav.Link as={Link} to="/courses">Courses</Nav.Link>
+              {showCourses && (
+                <div className="courses-dropdown">
+                  <Card className="course-card">
+                    <Card.Body>
+                      <Card.Title>Master of Studies (MSt)</Card.Title>
+                      <Card.Text>A comprehensive program focusing on interdisciplinary studies.</Card.Text>
+                    </Card.Body>
+                  </Card>
+                  <Card className="course-card">
+                    <Card.Body>
+                      <Card.Title>Master of Science (MSc)</Card.Title>
+                      <Card.Text>An advanced program with a focus on scientific research and practical applications.</Card.Text>
+                    </Card.Body>
+                  </Card>
+                </div>
+              )}
+            </Nav.Item>
+            <Nav.Link as={Link} to="/about" className="text-uppercase">About</Nav.Link>
+            <Nav.Link as={Link} to="/blog" className="text-uppercase">Blog</Nav.Link>
+            <Nav.Link as={Link} to="/contact" className="text-uppercase">Contact</Nav.Link>
+            <Nav.Link as={Link} to="/notifications" className="text-uppercase">Notifications</Nav.Link>
+            <Nav.Item
+              className="text-uppercase"
+              onMouseEnter={handleMouseEnterLogin}
+              onMouseLeave={handleMouseLeaveLogin}
+            >
+              <Nav.Link as={Link} to="/login">LogIn</Nav.Link>
+              {showLoginOptions && (
+                <div className="login-dropdown">
+                  <Button variant="primary" className="login-btn" >Student</Button>
+                  <Button variant="secondary" className="login-btn">Employee</Button>
+                </div>
+              )}
+            </Nav.Item>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 };
 
